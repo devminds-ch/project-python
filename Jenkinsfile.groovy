9a7618e376e728be8b1b85e78e5f45ceae7c7f01
andreas <andreas@devminds.ch>
2024-09-05 21:13:52 +0200

    Add scripted Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,78 @@
+node {
+    stage('Checkout SCM') {
+        checkout scm
+    }
+    stage('Agent Setup') {
+        // https://www.jenkins.io/doc/book/pipeline/docker/
+        customImage = docker.build("jenkins-python:latest",
+                                    "-f .devcontainer/Dockerfile ./")
+    }
+    customImage.inside {
+        stage('Cleanup') {
+            sh 'rm -rf dist build docs/_build'
+        }
+        stage('Build documentation') {
+            sh 'pip install -e .' // required for python_training_project.version
+            sh 'cd docs && pipenv run make html'
+            archiveArtifacts(
+                artifacts: 'docs/_build/html/**',
+                onlyIfSuccessful: true
+            )
+            publishHTML([
+                allowMissing: false,
+                alwaysLinkToLastBuild: false,
+                keepAll: false,
+                reportDir: 'docs/_build/html/',
+                reportFiles: 'index.html',
+                reportName: 'Documentation',
+                reportTitles: '',
+                useWrapperFileDirectly: true
+            ])
+        }
+        stage('Static code analysis') {
+            warnError('flake8 issues found') {
+                sh 'flake8 src/python_training_project --format=pylint > flake8.log'
+            }
+            warnError('pylint issues found') {
+                sh 'pylint --msg-template="{path}:{line}: [{msg_id}, {obj}] {msg} ({symbol})" src/python_training_project > pylint.log'
+            }
+            warnError('mypy issues found') {
+                sh 'mypy src/python_training_project > mypy.log'
+            }
+            recordIssues(
+                sourceCodeRetention: 'LAST_BUILD',
+                tools: [
+                    taskScanner(
+                        highTags: 'FIXME',
+                        includePattern: 'src/**/*.py',
+                        lowTags: 'HACK',
+                        normalTags: 'TODO'
+                    ),
+                    flake8(pattern: 'flake8.log'),
+                    pyLint(pattern: 'pylint.log'),
+                    myPy(pattern: 'mypy.log')
+                ]
+            )
+        }
+        stage('Build Python package') {
+            sh 'python -m build --wheel'
+            archiveArtifacts(
+                artifacts: 'dist/**/*.whl',
+                onlyIfSuccessful: true
+            )
+        }
+        stage('Test Python package') {
+            sh 'pip install -e .'
+            sh 'pytest'
+            junit(
+                testResults: 'report.xml'
+            )
+            recordCoverage(
+                tools: [
+                    [parser: 'JUNIT', pattern: 'report.xml'],
+                    [parser: 'COBERTURA', pattern: 'coverage.xml']
+                ]
+            )
+        }
+    }
+}
