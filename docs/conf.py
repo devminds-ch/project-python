88ebd7b6e7bf96cb128e15d2edb1ec32c58a01ef
andreas <andreas@devminds.ch>
2024-10-20 12:38:13 +0000

    Cleanup Python version handling

--- docs/conf.py
+++ docs/conf.py
@@ -2,21 +2,13 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import os
-import sys
-sys.path.append(os.path.abspath('../src'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Python Training Project by devminds GmbH'
-copyright = '2024, Andreas, Thomas'
 author = 'Andreas, Thomas'
-
-# Note: this requires the python_training_project package to be installed
-from python_training_project.version import version  # noqa
-# The full version, including alpha/beta/rc tags
-release = version
+copyright = '2024, Andreas, Thomas'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- src/python_training_project/__main__.py
+++ src/python_training_project/__main__.py
@@ -1,18 +1,18 @@
 """
 The main entry point contains the command line interface.
 """
-import click
 import logging
 import sys
 
+import click
+
 from python_training_project.calculate import sum
-from python_training_project.version import version
 
 log = logging.getLogger(__name__)
 
 
 @click.group()
-@click.version_option(version)
+@click.version_option()
 def cli():
     """Python Training Project by devminds GmbH."""
     logging.basicConfig(
@@ -26,7 +26,7 @@ def cli():
 @click.argument('b', type=float)
 def cli_sum(a: float, b: float):
     """Show the sum of two numbers on the console."""
-    log.info(f'Sum of {a} and {b} is {sum(a, b)}')
+    log.info('Sum of %f and %f is %f', a, b, sum(a, b))
 
 
 if __name__ == '__main__':
