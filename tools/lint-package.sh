1b7bb4c3dd33c45c497084dfc89ddc700906196f
Andreas Nussberger <andreas@devminds.ch>
2025-01-01 22:32:49 +0100

    Migrate from dev-dependencies to dependency-groups

--- tools/lint-package.sh
+++ tools/lint-package.sh
@@ -11,31 +11,31 @@ STATUS=0
 set +e
 
 echo "Running flake8..."
-uv run --all-extras flake8 src/python_training_project --format=pylint > build/flake8.txt
+flake8 src/python_training_project --format=pylint > build/flake8.txt
 if [ $? -ne 0 ]; then
     STATUS=1
 fi
 
 echo "Running mypy..."
-uv run --all-extras mypy src/python_training_project > build/mypy.txt
+mypy src/python_training_project > build/mypy.txt
 if [ $? -ne 0 ]; then
     STATUS=1
 fi
 
 echo "Running pylint..."
-uv run --all-extras pylint src/python_training_project --msg-template="{path}:{line}: [{msg_id}, {obj}] {msg} ({symbol})" > build/pylint.txt
+pylint src/python_training_project --msg-template="{path}:{line}: [{msg_id}, {obj}] {msg} ({symbol})" > build/pylint.txt
 if [ $? -ne 0 ]; then
     STATUS=1
 fi
 
 echo "Running ruff check..."
-uv run --all-extras ruff check > build/ruff.txt
+ruff check > build/ruff.txt
 if [ $? -ne 0 ]; then
     STATUS=1
 fi
 
 echo "Running ruff format..."
-uv run --all-extras ruff format --check
+ruff format --check
 if [ $? -ne 0 ]; then
     STATUS=1
 fi
