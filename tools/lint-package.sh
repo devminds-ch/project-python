7fb64d3f72293f1923c2b0060ee4842ce99a3799
Andreas Nussberger <andreas@devminds.ch>
2025-02-12 17:44:37 +0100

    Prefix binary commands with uv run to improve interoperability between different execution environments

--- tools/lint-package.sh
+++ tools/lint-package.sh
@@ -11,31 +11,31 @@ STATUS=0
 set +e
 
 echo "Running flake8..."
-flake8 src/python_training_project --format=pylint > build/flake8.txt
+uv run --all-groups flake8 src/python_training_project --format=pylint > build/flake8.txt
 if [ $? -ne 0 ]; then
     STATUS=1
 fi
 
 echo "Running mypy..."
-mypy src/python_training_project > build/mypy.txt
+uv run --all-groups mypy src/python_training_project > build/mypy.txt
 if [ $? -ne 0 ]; then
     STATUS=1
 fi
 
 echo "Running pylint..."
-pylint src/python_training_project --msg-template="{path}:{line}: [{msg_id}, {obj}] {msg} ({symbol})" > build/pylint.txt
+uv run --all-groups pylint src/python_training_project --msg-template="{path}:{line}: [{msg_id}, {obj}] {msg} ({symbol})" > build/pylint.txt
 if [ $? -ne 0 ]; then
     STATUS=1
 fi
 
 echo "Running ruff check..."
-ruff check > build/ruff.txt
+uv run --all-groups ruff check > build/ruff.txt
 if [ $? -ne 0 ]; then
     STATUS=1
 fi
 
 echo "Running ruff format..."
-ruff format --check
+uv run --all-groups ruff format --check
 if [ $? -ne 0 ]; then
     STATUS=1
 fi
