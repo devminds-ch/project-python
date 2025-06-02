2878336c54f11ab88a7d0ef0080f4b38f859a4c7
Andreas Nussberger <andreas@devminds.ch>
2025-06-02 09:43:29 +0200

    Move uv cache to repo root

--- tools/deploy-package.sh
+++ tools/deploy-package.sh
@@ -3,6 +3,10 @@
 # Get Git root directory
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
+# Force the uv cache directory to be located in the repository root
+# to avoid permission issues within CI/CD environments.
+export UV_CACHE_DIR="${REPO_ROOT}/.uv_cache"
+
 pushd "${REPO_ROOT}" 2>&1 > /dev/null
 
 echo "Deploying package..."
